52c7051e859113c9b718dcdbd4be4bb23a1b532a
99Page <nou0ggid@gmail.com>
2023-08-21 09:09:42 +0900

    inheritance

---
+++ SwiftDocumentation/14-Inheritance/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Inheritance
+//
+//  Created by wooyoung on 2023/08/21.
+//
+
+import Foundation
+
+//  MARK: Inheritance
+
+//  MARK: Defining a base class
+
+/*
+ 스위프트는 universal base class가 없다.
+ 자바의 Object같은거 
+ */
+
