1536c32cd2ecdce541edf7fd08f726d9aba36430
99Page <nou0ggid@gmail.com>
2023-08-30 10:29:34 +0900

    toggler tiemr button tapped

---
+++ TCAStudy/TCAStudy/Tour of TCA/1-The basics /CounterFeature.swift
@@ -0,0 +1,111 @@
+//
+//  CounterFeature.swift
+//  TCAStudy
+//
+//  Created by wooyoung on 2023/08/30.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct CounterFeature: Reducer {
+    struct State: Equatable {
+        static let maxCount = 10
+        
+        var count = 0
+        var fact: String?
+        var isTimerOn = false
+        var isLoadingFact = false
+        
+        var isMax: Bool {
+            count == CounterFeature.State.maxCount
+        }
+    }
+    
+    /*
+     UnitTest를 진행하기 위해서 Equatable 채택 필요
+     send에는 필요없지만 receive에 필요하다
+     */
+    enum Action: Equatable {
+        case decrementButtonTapped
+        case incrementButtonTapped
+        case getFactButtonTapped
+        case toggleTimerButtonTapped
+        case factResponse(String)
+        case timerTicking
+    }
+    
+    private enum CancellID: Hashable {
+        case timer
+    }
+    
+    //  의존성 주입이 필요할 때 PointFree에서 만든
+    //  Dependency 라이브러리 사용
+    //  테스트할 때 의존성을 설정해주지 않으면
+    //  에러가 발생한다. 
+    @Dependency(\.continuousClock) var clock
+    
+    //  Reducer<State, Action>을 RecuerOf<Self>로 축약
+    var body: some ReducerOf<Self> {
+        
+        //  Reduce는 Effect를 반환한다
+        //  반환한 Effect를 사용해 다른 action 재호출 가능
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                state.fact = nil
+                return .none
+            case .incrementButtonTapped:
+                state.count += 1
+                state.fact = nil
+                return .none
+            case .getFactButtonTapped:
+                state.fact = nil
+                state.isLoadingFact = true
+                return .run { [count = state.count] send in // 내부 state 값을 capture해야 read 가능
+                    try await clock.sleep(for: .seconds(2))
+                    let url = URL(string: "http://www.numbersapi.com/\(count)")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    print(fact)
+                    
+                    // 클로저 내부에서 불가능
+                    // mutate를 하고 싶다면 관련된 다른 action을 실행해야함
+                    // state.fact = fact
+                    // 이를 처리하기 위해서 factResponse action 생성
+                    // await을 이용해서 호출해야함
+                    await send(.factResponse(fact))
+                }
+            case .toggleTimerButtonTapped:
+                state.isTimerOn.toggle()
+                
+                if state.isTimerOn {
+                    return .run { send in
+                        for await _ in clock.timer(interval: .seconds(1)) {
+                            await send(.timerTicking)
+                        }
+                    }
+                    .cancellable(id: CancellID.timer) // 실행 중인 비동기 함수를 종료시키려면 cancellable 정의 필요
+                } else {
+                    return .cancel(id: CancellID.timer) // 비동기 함수 종료할 때 .cancel 및 동일할 hashable 값 호출
+                }
+        
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoadingFact = false
+                return .none
+                
+            case .timerTicking:
+                state.count += 1
+                
+                if state.isMax {
+                    state.isTimerOn = false
+                    return .cancel(id: CancellID.timer)
+                } else {
+                    return .none
+                }
+            }
+        }
+    }
+}
---
+++ TCAStudy/TCAStudyTests/Tour of TCA/TheBasicsTests.swift
@@ -0,0 +1,92 @@
+//
+//  TheBasicsTests.swift
+//  TCAStudyTests
+//
+//  Created by wooyoung on 2023/08/30.
+//
+
+import XCTest
+import ComposableArchitecture
+@testable import TCAStudy
+
+@MainActor
+final class TheBasicsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    //  MVVM에서 메소드를 테스트한거처럼 TCA에서는 Action을 테스트
+    //  모든 action은 비동기 메소드
+    //  따라서 모든 test는 async
+    func test_IncrementButtonTapped() async {
+        
+        let repeatCount: Int = .random(in: 1..<100)
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        //  stress test
+        for i in 1...repeatCount {
+            await store.send(.incrementButtonTapped, assert: { state in
+                //  내부에 테스트할 내용이 들어간다.
+                state.count = i
+            })
+        }
+    }
+    
+    func test_DecrementButtonTapped() async {
+        
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        let expected = store.state.count - 1
+        
+        await store.send(.decrementButtonTapped, assert: { state in
+            state.count = expected
+        })
+    }
+    
+    func test_ToggleTimerButtonTapped() async throws {
+        
+        /*
+         Task로 시간을 설정하면 실제 동작하는 코드와 테스트에서 기다리는 Task의 시간이 공유되지 않는다.
+         Clock을 사용해서 양쪽의 동기를 맞춰줄 수 있다.
+         */
+        let clock = TestClock()
+        
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        /*
+         toggleTimerButtonTapped를 호출하면 내부에서
+         timerTicking Action을 실행한다.
+         모든 테스트는 실행되고 있는 Action이 종료되어야 하는데
+         toggleTimerButtonTapped를 한번만 호출하면 Action이 여전히 실행중이여서
+         테스트에 실패한다.
+         */
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerOn = true
+        }
+        
+        await clock.advance(by: .seconds(1))
+        
+        //  호출된 모든 Action은 테스트되어야 한다.
+        //  timcerTicking은 toggleTimerButtonTapped를 통해서 호출된다. 
+        await store.receive(.timerTicking) {
+            $0.count = 1
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerOn = false
+        }
+    }
+}
