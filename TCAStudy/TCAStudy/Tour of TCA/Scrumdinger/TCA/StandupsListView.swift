8f9f1e64847d8ae12d3646cf7120ad1239eda4ff
99Page <nou0ggid@gmail.com>
2023-09-24 14:05:19 +0900

    tour 3 ongoing

--- TCAStudy/TCAStudy/Tour of TCA/Scrumdinger/TCA/StandupsListView.swift
+++ TCAStudy/TCAStudy/Tour of TCA/Scrumdinger/TCA/StandupsListView.swift
@@ -9,23 +9,51 @@ import ComposableArchitecture
 import SwiftUI
 
 struct StandupsListFeature: Reducer {
+    
+    // MVVM에서 @Published를 사용하는 부분이 State와 매칭된다.
+    // 나는 @Published를 한개만 선언하는데 이곳에서는 여러개를 선언한다는 차이점이 있다
     struct State {
+        @PresentationState var addStandup: StandupFormFeature.State?
         var standups: IdentifiedArrayOf<Standup> = []
     }
     
     enum Action {
         case addButtonTapped
+        
+        // 하위로 전달한 Action의 동작이 실행된 후 나중에 실행된다.
+        // Delegate 패턴이 필요할 때 유용하다
+        case addStandup(PresentationAction<StandupFormFeature.Action>)
+        case cancelStandupButtonTapped
+        case saveStandupButtonTapped
     }
+    
+    @Dependency(\.uuid) var uuid
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.standups.append(
-                    Standup(id: UUID(), theme: .allCases.randomElement()!)
-                )
+                state.addStandup = StandupFormFeature.State(standup: Standup(id: uuid()))
+                return .none
+                
+            case .addStandup(_):
+                return .none
+                
+            case .cancelStandupButtonTapped:
+                state.addStandup = nil
+                return .none
+                
+            case .saveStandupButtonTapped:
+                guard let addStandup = state.addStandup?.standup
+                else { return .none }
+                
+                state.standups.append(addStandup)
+                state.addStandup = nil
                 return .none
             }
+        }
+        .ifLet(\.$addStandup, action: /Action.addStandup) {
+            StandupFormFeature()
         }
     }
 }
@@ -53,6 +81,31 @@ struct StandupsListView: View {
                     Button("add") {
                         viewStore.send(.addButtonTapped)
                     }
+                }
+            }
+            .sheet(store: store.scope(state: \.$addStandup,
+                                      action: { .addStandup($0) })
+            ) { store in
+                NavigationStack {
+                    StandupFormView(store: store)
+                        .navigationTitle("New standup")
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                Button {
+                                    viewStore.send(.saveStandupButtonTapped)
+                                } label: {
+                                    Text("save")
+                                }
+                            }
+                            
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    viewStore.send(.cancelStandupButtonTapped)
+                                } label: {
+                                    Text("cancel")
+                                }
+                            }
+                        }
                 }
             }
         }
