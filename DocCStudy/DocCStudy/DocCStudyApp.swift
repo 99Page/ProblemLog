0f1b0deafe4763542d05bf1e4f7ab4f7f60c5a06
page <nou0ggid@gmail.com>
2023-09-26 09:04:58 +0900

    Meet DocC documentation in Xcode

---
+++ DocCStudy/DocCStudy/DocCStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocCStudyApp.swift
+//  DocCStudy
+//
+//  Created by wooyoung on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct DocCStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
