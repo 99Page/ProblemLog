e2bdee2a9a2ad31454a26dd21a4e591992513a76
page <nou0ggid@gmail.com>
2023-10-10 13:01:26 +0900

    popup

---
+++ PopupStudy/PopupStudy/PopupModel.swift
@@ -0,0 +1,13 @@
+//
+//  PopupModel.swift
+//  PopupStudy
+//
+//  Created by 노우영 on 2023/10/09.
+//
+
+import Foundation
+
+struct PopupModel: Identifiable {
+    let id: String = UUID().uuidString
+    let title: String = "Title"
+}
