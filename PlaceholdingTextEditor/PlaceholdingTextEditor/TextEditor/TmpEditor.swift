2e5fecafe7263b2ccf9ae1a1517982c9ebfa7098
page <nou0ggid@gmail.com>
2023-10-25 11:00:53 +0900

    Bining 문제 해결

--- PlaceholdingTextEditor/PlaceholdingTextEditor/TextEditor/TmpEditor.swift
+++ PlaceholdingTextEditor/PlaceholdingTextEditor/TextEditor/TmpEditor.swift
@@ -17,10 +17,27 @@ struct TmpEditor: UIViewRepresentable {
         textView.textContainer.lineFragmentPadding = 0
         textView.textContainerInset = .zero
         textView.font = font
+        textView.delegate = context.coordinator
         return textView
     }
     
     func updateUIView(_ uiView: UITextView, context: Context) {
         uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator($text)
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var text: Binding<String>
+        
+        init(_ text: Binding<String>) {
+            self.text = text
+        }
+        
+        func textViewDidChange(_ textView: UITextView) {
+            text.wrappedValue = textView.text
+        }
     }
 }
