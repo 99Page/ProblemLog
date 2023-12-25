15e8c35b433a14c1756ad62e53ba6ed266825496
99Page <nou0ggid@gmail.com>
2023-12-25 20:21:56 +0900

    localization

---
+++ Projects/Localization/Sources/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 노우영 on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
